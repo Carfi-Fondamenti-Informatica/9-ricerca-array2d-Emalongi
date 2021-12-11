55c32e017030c883c5d3ff08b22801d2d9d2f5ba
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-12-11 11:18:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int funzione(char nomi[10][20],char nome[]);
+int main() {
+    char lista[10][20]={0};
+    char nome[20]={0};
+    cout<<"l'utente scelga dieci nomi:"<<endl;
+
+    for(size_t i=0;i<10;i++) cin >> lista[i];
+
+    cout<<"ancora uno:";
+    for(int j=0;j<20;j++) nome[j];
+    cin >> nome;
+
+    int posizione = funzione(lista ,nome);
+    if(posizione>=0){
+        cout<< posizione << endl;
+    }else{
+        cout <<"non presente"<< endl;
+    }
 }
+
+
