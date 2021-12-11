06a0aa411f7638daa0ce9ef46bd5a02d919eca2c
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-12-11 11:16:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include"lib.h"
+int funzione(char lista[10][20],char nome[20]){
+    for(int i=0;i<10;i++){
+        bool t=true;
+        for (int j=0;j<20;j++){
+            if(lista[i][j]!=nome[j]) t=false;
+        }
+        if(t) return i;
+    }
+    return -1;
+
+}
 
